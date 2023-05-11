3bc3d7dec395af993e5d0a7e97d01ee504f19bff
shaxobiddin <shaxobiddins00@gmail.com>
2023-05-11 14:12:24 +0500

    Freezed bloc and json annotation added

---
+++ lib/pages/models/user_model/user_model.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'user_model.freezed.dart';
+part 'user_model.g.dart';
+
+@freezed
+class User with _$User {
+  factory User({
+    required String gender,
+    required Name name,
+    required String email,
+    required String phone,
+  }) = _User;
+  factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+}
+
+@freezed
+class Name with _$Name {
+  factory Name({required String title, required String first, required String last}) = _Name;
+
+  factory Name.fromJson(Map<String, Object?> json) => _$NameFromJson(json);
+}
+
+@freezed
+class Picture with _$Picture {
+  factory Picture({required String large, required String medium, required String thumbnail}) = _Picture;
+
+  factory Picture.fromJson(Map<String, Object?> json) => _$PictureFromJson(json);
+}
